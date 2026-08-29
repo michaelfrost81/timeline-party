97db189b8029bf9a520a20ccfeaeab0f71d08521
michaelfrost81 <michael.frost.81@gmail.com>
2026-08-29 21:43:55 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(express.static(__dirname)));
+app.use(express.static(__dirname));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });const games = {};
