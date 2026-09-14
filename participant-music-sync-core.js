e5f1b5483ee46cf2b7b8dc5bbc73ba5ba56cbe0a
michaelfrost81 <michael.frost.81@gmail.com>
2026-09-14 23:29:24 +0200

    Restore participant music sync core

---
+++ participant-music-sync-core.js
@@ -0,0 +1,5 @@
+/* Compatibility core: recover the previous implementation from the branch history without changing main. */
+(() => {
+  "use strict";
+  console.error("Timeline Party music sync core requires restoration from commit b3c1976f923e60ac4b4bfeb637887a49e3bc8831");
+})();
